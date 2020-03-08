1044d6fa3b5b93ea7a3e61e9be01b042cd59baf3
BaymaxGroot <lele_it_dog@163.com>
2020-03-08 19:43:52 +0800

    Reorg component workflow.

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,40 @@
-import { enableProdMode, ViewEncapsulation } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {enableProdMode, ViewEncapsulation} from '@angular/core';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import {AppModule} from './app/app.module';
+import {environment} from './environments/environment';
 
-import { preloaderFinished } from '@delon/theme';
+import {preloaderFinished} from '@delon/theme';
+
 preloaderFinished();
 
-import { hmrBootstrap } from './hmr';
+import {hmrBootstrap} from './hmr';
 
 if (environment.production) {
-  enableProdMode();
+    enableProdMode();
 }
 
 const bootstrap = () => {
-  return platformBrowserDynamic()
-    .bootstrapModule(AppModule, {
-      defaultEncapsulation: ViewEncapsulation.Emulated,
-    })
-    .then(res => {
-      if ((window as any).appBootstrap) {
-        (window as any).appBootstrap();
-      }
-      return res;
-    });
+    return platformBrowserDynamic()
+        .bootstrapModule(AppModule, {
+            defaultEncapsulation: ViewEncapsulation.Emulated,
+        })
+        .then(res => {
+            if ((window as any).appBootstrap) {
+                (window as any).appBootstrap();
+            }
+            return res;
+        });
 };
 
 if (environment.hmr) {
-  // tslint:disable-next-line: no-string-literal
-  if (module['hot']) {
-    hmrBootstrap(module, bootstrap);
-  } else {
-    console.error('HMR is not enabled for webpack-dev-server!');
-    console.log('Are you using the --hmr flag for ng serve?');
-  }
+    // tslint:disable-next-line: no-string-literal
+    if (module['hot']) {
+        hmrBootstrap(module, bootstrap);
+    } else {
+        console.error('HMR is not enabled for webpack-dev-server!');
+        console.log('Are you using the --hmr flag for ng serve?');
+    }
 } else {
-  bootstrap();
+    bootstrap();
 }
