da2f3e73bfb64f5cf89d9bfe4c5c67831d80caf0
BaymaxGroot <lele_it_dog@163.com>
2020-03-14 14:42:18 +0800

    Reorg token & login logic.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-    SERVER_URL: `./`,
-    AUTH: '',
+    SERVER_URL: `http://localhost:8993/api`,
+    AUTH: 'E^8P56AUvAMPv8wrE@RAaxzb8^PMcj47VbfB3N&w8a$s2n%C4#U3zar?P[LR2mt',
     production: false,
     useHash: false,
     hmr: false,
