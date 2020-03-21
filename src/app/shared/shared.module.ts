c1a7fb15689306abdd6bd93d0c5b138cb11cf08c
BaymaxGroot <lele_it_dog@163.com>
2020-03-21 22:40:14 +0800

    work on management.component.html management.component.ts

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,59 +1,64 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ReactiveFormsModule, FormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
 // delon
-import { AlainThemeModule } from '@delon/theme';
-import { DelonABCModule } from '@delon/abc';
-import { DelonACLModule } from '@delon/acl';
-import { DelonFormModule } from '@delon/form';
+import {AlainThemeModule} from '@delon/theme';
+import {DelonABCModule} from '@delon/abc';
+import {DelonACLModule} from '@delon/acl';
+import {DelonFormModule} from '@delon/form';
 
 // #region third libs
-import { NgZorroAntdModule } from 'ng-zorro-antd';
-import { CountdownModule } from 'ngx-countdown';
+import {NgZorroAntdModule} from 'ng-zorro-antd';
+import {CountdownModule} from 'ngx-countdown';
+import {UEditorModule} from "ngx-ueditor";
+
 const THIRDMODULES = [
-  NgZorroAntdModule,
-  CountdownModule
+    NgZorroAntdModule,
+    CountdownModule,
+    UEditorModule
 ];
 // #endregion
 
 // #region your componets & directives
 const COMPONENTS = [];
 const DIRECTIVES = [];
+
 // #endregion
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    RouterModule,
-    ReactiveFormsModule,
-    AlainThemeModule.forChild(),
-    DelonABCModule,
-    DelonACLModule,
-    DelonFormModule,
-    // third libs
-    ...THIRDMODULES
-  ],
-  declarations: [
-    // your components
-    ...COMPONENTS,
-    ...DIRECTIVES
-  ],
-  exports: [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
-    RouterModule,
-    AlainThemeModule,
-    DelonABCModule,
-    DelonACLModule,
-    DelonFormModule,
-    // third libs
-    ...THIRDMODULES,
-    // your components
-    ...COMPONENTS,
-    ...DIRECTIVES
-  ]
+    imports: [
+        CommonModule,
+        FormsModule,
+        RouterModule,
+        ReactiveFormsModule,
+        AlainThemeModule.forChild(),
+        DelonABCModule,
+        DelonACLModule,
+        DelonFormModule,
+        // third libs
+        ...THIRDMODULES
+    ],
+    declarations: [
+        // your components
+        ...COMPONENTS,
+        ...DIRECTIVES
+    ],
+    exports: [
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        RouterModule,
+        AlainThemeModule,
+        DelonABCModule,
+        DelonACLModule,
+        DelonFormModule,
+        // third libs
+        ...THIRDMODULES,
+        // your components
+        ...COMPONENTS,
+        ...DIRECTIVES
+    ]
 })
-export class SharedModule { }
+export class SharedModule {
+}
