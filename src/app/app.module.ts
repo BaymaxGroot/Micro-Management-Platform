fa858d9e13a1b73b5c5307b2bf72722279720230
BaymaxGroot <lele_it_dog@163.com>
2020-03-22 01:17:00 +0800

    work on management.component.html management.component.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -71,6 +71,7 @@ import {SharedModule} from '@shared';
 import {AppComponent} from './app.component';
 import {RoutesModule} from './routes/routes.module';
 import {LayoutModule} from './layout/layout.module';
+import {UEditorModule} from "ngx-ueditor";
 
 @NgModule({
     declarations: [
@@ -81,6 +82,13 @@ import {LayoutModule} from './layout/layout.module';
         BrowserAnimationsModule,
         HttpClientModule,
         DelonModule.forRoot(),
+        UEditorModule.forRoot({
+            // **注：** 建议使用本地路径；以下为了减少 ng-alain 脚手架的包体大小引用了CDN，可能会有部分功能受影响
+            js: [`//apps.bdimg.com/libs/ueditor/1.4.3.1/ueditor.config.js`, `//apps.bdimg.com/libs/ueditor/1.4.3.1/ueditor.all.min.js`],
+            options: {
+                UEDITOR_HOME_URL: `//apps.bdimg.com/libs/ueditor/1.4.3.1/`,
+            },
+        }),
         CoreModule,
         SharedModule,
         LayoutModule,
