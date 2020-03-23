68b269cae59f4e2a51302d6170794f0c38194906
BaymaxGroot <lele_it_dog@163.com>
2020-03-24 00:08:48 +0800

    work on order-item

---
+++ src/app/routes/order/list/order-item/order-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'micro-order-item',
+  templateUrl: './order-item.component.html',
+  styleUrls: ['./order-item.component.less']
+})
+export class OrderItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
