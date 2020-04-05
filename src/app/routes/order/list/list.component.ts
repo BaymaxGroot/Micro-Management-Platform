1c533a5e1f6f2856e2f7591e5c23f196fa2feda1
BaymaxGroot <lele_it_dog@163.com>
2020-04-06 00:45:48 +0800

    add print order component

--- src/app/routes/order/list/list.component.ts
+++ src/app/routes/order/list/list.component.ts
@@ -1,9 +1,11 @@
 import {Component, OnInit} from '@angular/core';
 import {STColumn, STData} from "@delon/abc";
-import {NzMessageService} from "ng-zorro-antd";
+import {NzMessageService, NzNotificationService} from "ng-zorro-antd";
 import {MicroAppService} from "@core/net/micro-app.service";
 import {Interface} from "../../../lib/enums/interface.enum";
 import {SFComponent, SFSchema} from "@delon/form";
+import {Lodop, LodopService} from "@delon/abc";
+import {toTitleCase} from "codelyzer/util/utils";
 
 @Component({
     selector: 'micro-list',
@@ -16,9 +18,12 @@ export class ListComponent implements OnInit {
     dateFormat = 'yyyy/MM/dd';
 
     constructor(
+        public lodopSrv: LodopService,
         private msg: NzMessageService,
         private _microAppHttpClient: MicroAppService,
+        private notify: NzNotificationService
     ) {
+
     }
 
     ngOnInit() {
@@ -110,7 +115,7 @@ export class ListComponent implements OnInit {
                 type: 'string',
                 title: '收件人'
             },
-            phone:{
+            phone: {
                 type: 'string',
                 title: '电话'
             },
@@ -148,5 +153,92 @@ export class ListComponent implements OnInit {
             this.isChangingAddress = false;
             this.msg.error('修改订单收货地址失败!');
         })
+    }
+
+    cog: any = {
+        url: 'https://localhost:8443/CLodopfuncs.js',
+        printer: '',
+        paper: '',
+        html: '',
+    };
+    isPrintOrder: boolean = false;
+    error = false;
+    lodop: Lodop | null = null;
+    pinters: any[] = [];
+    papers: string[] = [];
+
+    printing = false;
+
+    printOrder(value: any, shop: string) {
+        this.isPrintOrder = true;
+
+        let order = value['order_number'];
+        let mdate = value['date'];
+        let account = value['member_name'];
+        let expressinfo = value['express_info'];
+
+        let content = '';
+        let sum = 0;
+
+        value.goods_list[shop].forEach(item => {
+           content += `
+               商品名称: ${item.name}
+               购买数量: ${item.quantity}
+               单价: ${item.price}元
+           `;
+        });
+
+        let mhtml = `
+            订单编号: ${order}
+            下单时间: ${mdate}
+            下单账户: ${account}
+
+            ${content}
+
+            收件人: ${expressinfo.nickname}
+            电话: ${expressinfo.mobile}
+            收件地址: ${expressinfo.address}
+        `;
+
+        this.cog.html = mhtml;
+        this.lodopSrv.lodop.subscribe(({lodop, ok}) => {
+            if (!ok) {
+                this.error = true;
+                return;
+            }
+            this.error = false;
+            this.msg.success(`打印机加载成功`);
+            this.lodop = lodop as Lodop;
+            this.pinters = this.lodopSrv.printer;
+        });
+    }
+
+    reload(options: any = {url: 'https://localhost:8443/CLodopfuncs.js'}) {
+        this.pinters = [];
+        this.papers = [];
+        this.cog.printer = '';
+        this.cog.paper = '';
+
+        this.lodopSrv.cog = {...this.cog, ...options};
+        this.error = false;
+        if (options === null) this.lodopSrv.reset();
+    }
+
+    changePinter(name: string) {
+        this.papers = this.lodop!.GET_PAGESIZES_LIST(name, '\n').split('\n');
+    }
+
+    print(isPrivew = false) {
+        const LODOP = this.lodop as Lodop;
+        LODOP.PRINT_INITA(10, 20, 810, 610, '测试C-Lodop远程打印四步骤');
+        LODOP.SET_PRINTER_INDEXA(this.cog.printer);
+        LODOP.SET_PRINT_PAGESIZE(0, 0, 0, this.cog.paper);
+        LODOP.ADD_PRINT_TEXT(1, 1, 300, 200, '');
+        LODOP.ADD_PRINT_TEXT(20, 10, '90%', '95%', this.cog.html);
+        LODOP.SET_PRINT_STYLEA(0, 'ItemType', 4);
+        LODOP.NewPageA();
+        LODOP.ADD_PRINT_HTM(20, 10, '90%', '95%', this.cog.html);
+        if (isPrivew) LODOP.PREVIEW();
+        else LODOP.PRINT();
     }
 }
