a02b43e82d7cfc674913b0115ff20f1fa17a278f
BaymaxGroot <lele_it_dog@163.com>
2020-04-05 23:40:11 +0800

    work on system params setting component.

--- src/app/routes/setting/params/params.component.ts
+++ src/app/routes/setting/params/params.component.ts
@@ -18,23 +18,36 @@ export class ParamsComponent implements OnInit {
     }
 
     ngOnInit() {
+        this.loadSettingParams();
+    }
+
+    isLoading: boolean = false;
+    loadSettingParams() {
+        this.isLoading = true;
+        this._microAppHttpClient.get(Interface.GetParamsEndPoint).subscribe((data) => {
+            this.formData = data;
+            this.isLoading = false;
+        }, (err) => {
+            this.msg.error('获取配置参数出错!');
+        })
     }
 
     isSettingParams: boolean = false;
+    formData: any;
     settingSchema: SFSchema = {
         properties: {
-            cost: {
+            min_price: {
                 type: 'number',
                 title: '订单金额',
                 ui: {
                     optionalHelp: '订单金额超过此值，免运费'
                 }
             },
-            deliver: {
+            yunfei: {
                 type: 'number',
                 title: '运费'
             },
-            timer: {
+            refund_hour: {
                 type: 'integer',
                 title: '时间',
                 ui: {
@@ -43,19 +56,24 @@ export class ParamsComponent implements OnInit {
                 }
             }
         },
-        required: ['cost', 'deliver', 'timer']
+        required: ['min_price', 'yunfei', 'refund_hour']
     };
 
     disableChangeSettingParamsSubmitButton(sf: SFComponent) {
         return !sf.valid;
     }
 
-    handleSubmit(value: any) {
-        let changeSettingTemplate = {};
+    handleSubmit(e: any) {
+        let changeSettingTemplate = {
+            yunfei: e['yunfei'],
+            refund_hour: e['refund_hour'],
+            min_price: e['min_price']
+        };
         this.isSettingParams = true;
         this._microAppHttpClient.post(Interface.SettingParamsEndPoint, changeSettingTemplate).subscribe((data) => {
             this.isSettingParams = false;
             this.msg.info('设置更新成功!');
+            this.loadSettingParams();
         }, (err) => {
             this.isSettingParams = false;
             this.msg.info('设置更新失败!');
